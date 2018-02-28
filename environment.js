4f960b108763018ddcb1f6d3f46915088d6b3a0f
Kepler Sticka-Jones <kepler+git@stickajones.org>
2018-02-27 19:09:35 -0700

    fix: enable package after loading, throw if no package is found

--- environment.js
+++ environment.js
@@ -10,12 +10,17 @@ class AtomEnvironment extends JsdomEnvironment {
   async setup() {
     await super.setup();
     this.global.atom = global.atom;
-    
+
     // Naively assume the current working directory is the package we want to
     //    test and use a private method from Atom PackageManager to load it.
     //
     // This is bad and hacky. I'm sorry.
-    this.global.atom.packages.loadPackage(process.cwd());
+    this.testedPackage = this.global.atom.packages.loadPackage(process.cwd());
+
+    // And of course we have to activate it ourselves.
+    //
+    // I'm still very sorry.
+    await this.testedPackage.activate();
   }
 }
 
